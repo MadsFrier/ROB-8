27e2d376ebc3ee2f8a5a33020fba462ffd7eefc3
shauga20 <shauga20@student.aau.dk>
2024-03-21 13:39:05 +0100

    Added robot_pose_1 as test

--- rosbag_data/scripts/pose_rosbag.py
+++ rosbag_data/scripts/pose_rosbag.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-#!/usr/bin/env python3
 import rosbag
 import numpy as np
 
@@ -13,7 +12,7 @@ def extract_robot_positions_from_bag(bag_file):
         for topic, msg, t in bag.read_messages(topics=['/robot_pose']):
             save_robot_pose_to_file(msg.position, msg.orientation, count)
             count += 1
-            print(msg)
+            #print(msg)
 
 if __name__ == "__main__":
     bag_file = "2024-03-20-13-57-17.bag"
